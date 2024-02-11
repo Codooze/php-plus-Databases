f812822825d36fdc328f0551b6512d9e3d105f3f
Jeison 🌱 <jeysonm021@gmail.com>
2024-02-10 20:41:31 -0500

    Add comments explaining form fields and hidden input usage in create.php

--- resources/views/incomes/create.php
+++ resources/views/incomes/create.php
@@ -11,6 +11,11 @@
 <body>
     <h1>Agrega un nuevo gasto</h1>
     <main>
+    <!-- en el case income en public/index.php se establece el metodo por post, y segun el RouterHandler.php si el case es post va a usar store() para crear el nuevo registro 
+    el "name" es importante que coincida con los campos de la base de datos, (ver IncomesController.php)
+
+    the hidden input is to send the method to the RouterHandler.php, so it can be used to determine the method to use in the switch statement
+-->
         <form action="/incomes" method="post">
             
             <div class="input-group">
